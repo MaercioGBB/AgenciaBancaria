afe368516b1e623f8ccdde9c1769d64d76a1b162
Maercio G B Brandao <maerciogbb@gmail.com>
2019-07-11 12:44:10 -0300

    Projeto Banco completo

---
+++ Conta.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+
+namespace banco
+{
+    class Conta
+    {
+        public int Numero { get; private set; }
+        public string Nome { get; set; }
+        public float Saldo { get; private set; }
+
+        public Conta(int numero, string nome)
+        {
+            Numero = numero;
+            Nome = nome;
+        }
+
+        public Conta(int numero, string nome, float valor) : this(numero, nome)
+        {
+            Saldo = valor;
+        }
+
+        public void Depositar(float valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Sacar(float valor)
+        {
+            Saldo -= valor;
+            Saldo -= 5.00f;
+        }
+
+        public override string ToString()
+        {
+            return "Numero: " + Numero
+                + ",\nNome: " + Nome
+                + ", Saldo: R$ " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta c;
+            Console.Write("Numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Titular: ");
+            string nome = Console.ReadLine();
+            Console.Write("Vai depositar (s/n): ");
+            char opcao = char.Parse(Console.ReadLine());
+            if(opcao == 's' || opcao == 'S')
+            {
+                Console.Write("Qual valor: ");
+                float valor = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                c = new Conta(numero, nome, valor);
+            }
+            else
+            {
+                c = new Conta(numero, nome);
+            }
+
+            Console.WriteLine(c);
+            Console.Write("\nValor deposito: ");
+            float val = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Depositar(val);
+            Console.WriteLine(c);
+            Console.Write("\nValor saque: ");
+            val = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Sacar(val);
+            Console.WriteLine("\nDados da conta: ");
+            Console.WriteLine(c);
+        }
+    }
+}
